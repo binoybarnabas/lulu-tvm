ff1f3706d7fb1ee6db012ee54bb4085ec4610c72
Fazil S <fazil.s@experionglobal.local>
2023-11-08 15:33:43 +0530

    fix:nav bar hamburger complete

--- scripts/header/test.js
+++ scripts/header/test.js
@@ -1,4 +1,16 @@
-const blueDivContentArray =  ["About Us", "Lulu International","E-Magazine","Services","Floor Plan","Careers","Licenses & Permits","Mall Timings","Contact Us"];
+const blueDivContentArray = [
+  "About Us",
+  "Lulu International",
+  "E-Magazine",
+  "Services",
+  "Floor Plan",
+  "Careers",
+  "Licenses & Permits",
+  "Mall Timings",
+  "Contact Us",
+];
+
+const links = ["https://facebook.com/", "test.com"];
 let blueFlag = true;
 
 function myFunction(x) {
@@ -10,20 +22,20 @@ function myFunction(x) {
     let blueContentDiv = document.createElement("div");
     blueContentDiv.classList.add("blue-content-div");
     blueDivContentArray.forEach((x) => {
-      let h3 = document.createElement("h3");
-      h3.classList.add("remove");
-    h3.textContent = x;
-      blueContentDiv.appendChild(h3);
-      div.appendChild(blueContentDiv)
-      
-    })
+      let blueHrefDiv = document.createElement("div");
+      blueHrefDiv.classList.add("blueContentDiv");
+      let a = document.createElement("a");
+      a.classList.add("remove");
+      a.textContent = x;
+      blueHrefDiv.appendChild(a);
+      blueContentDiv.appendChild(blueHrefDiv);
+      div.appendChild(blueContentDiv);
+    }),
     blueFlag = false;
-  }
-  else {
-    document.querySelectorAll(".remove").forEach(x => x.remove());
+  } else {
+    document.querySelectorAll(".remove").forEach((x) => x.remove());
+    document.querySelectorAll(".blue-content-div").forEach((x) => x.remove());
+
     blueFlag = true;
   }
-
-  
-
 }
