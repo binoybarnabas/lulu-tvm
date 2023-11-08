8e4c1b2b1f0c26f1fde30b3b6c269a581d331171
Fazil S <fazil.s@experionglobal.local>
2023-11-08 13:25:49 +0530

    fix:added js for blue box contents

--- scripts/header/test.js
+++ scripts/header/test.js
@@ -1,8 +1,29 @@
+const blueDivContentArray =  ["About Us", "Lulu International","E-Magazine","Services","Floor Plan","Careers","Licenses & Permits","Mall Timings","Contact Us"];
+let blueFlag = true;
+
 function myFunction(x) {
-    x.classList.toggle("change");
-    let div = document.querySelector('.blue');
-    div.classList.toggle('blue-box-active');
+  x.classList.toggle("change");
+  let div = document.querySelector(".blue");
+  div.classList.toggle("blue-box-active");
+
+  if (blueFlag == true) {
+    let blueContentDiv = document.createElement("div");
+    blueContentDiv.classList.add("blue-content-div");
+    blueDivContentArray.forEach((x) => {
+      let h3 = document.createElement("h3");
+      h3.classList.add("remove");
+    h3.textContent = x;
+      blueContentDiv.appendChild(h3);
+      div.appendChild(blueContentDiv)
+      
+    })
+    blueFlag = false;
   }
+  else {
+    document.querySelectorAll(".remove").forEach(x => x.remove());
+    blueFlag = true;
+  }
+
   
-  
-  
+
+}
