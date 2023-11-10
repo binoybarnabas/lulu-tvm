dbd2a0a3850c951ca4465ea189562d92bba558cb
Dencymol Baby <dencymol.baby@experionglobal.local>
2023-11-10 18:19:10 +0530

    feat:- added movies page and inner movie pages

--- scripts/homePage/home.js
+++ scripts/homePage/home.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+var swipere = new Swiper(".mySwiper", {
   cssMode: true,
   autoplay: {
     delay: 5000,
@@ -6,9 +6,7 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   slidersPerView: "auto",
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -19,113 +17,61 @@ var swiper = new Swiper(".mySwiper", {
 
 // Fetch data from the API
 
-// const request = fetch("https://dummyjson.com/users/");
-
-// const eventSectionDescription = document.querySelector(".eventSectionDesc");
-
-// request
-//   .then((response) => response.json())
-//   .then((data) => {
-//     data.users.forEach((element) => {
-//       const div1 = document.createElement("div");
-//       div1.classList.add("eventSectionDescSub1", "swiper-slide");
-
-//       console.log("hello");
-//       const div2 = document.createElement("div");
-//       div2.classList.add("eventSectionDescSub1-div");
-
-//       const div3 = document.createElement("div");
-//       div3.classList.add("eventSectionDescSub2");
-
-//       const h3 = document.createElement("h3");
-//       const a = document.createElement("a");
-
-//       const button = document.createElement("button");
-
-//       const i = document.createElement("i");
-//       i.classList.add("fa-solid", "fa-book");
-
-//       const span = document.createElement("span");
-//       span.textContent = "JOIN EVENT";
-
-//       button.appendChild(i);
-//       button.appendChild(span);
-//       a.appendChild(button);
-//       div3.appendChild(h3);
-//       div3.appendChild(a);
-//       div1.appendChild(div2);
-//       div1.appendChild(div3);
-//       eventSectionDescription.appendChild(div1);
-//     });
-//   })
-//   .catch((error) => console.log(error.message));
-
-// // event swiper
-
-// var swiper = new Swiper(".event-mySwiper", {
-//   pagination: ".swiper-pagination",
-//   direction: "horizontal",
-//   slidesPerView: 3,
-//   paginationClickable: true,
-//   spaceBetween: 30,
-//   mousewheelControl: true,
-//   parallax: true,
-//   speed: 600,
-//   autoplay: {
-//     delay: 5000,
-//   },
-
-//   loop: true,
-// });
-
-// Fetch data from the API
-
 const request = fetch("https://dummyjson.com/users/");
-
 const specialistImgSet = document.querySelector(".eventSectionDesc");
+
+// Create a swiperParams object to store the swiper settings
+const swiperParams = {
+  slidesPerView: 3, // Number of slides per view
+  // slidersPerView: "auto",
+  autoplay: {
+    delay: 5000,
+  },
+  spaceBetween: 70, // Space between slides in px
+  loop: true, // Enable loop mode
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  cssMode: true,
+};
+
+// Initialize the swiper instance with the swiperParams object
+const swiper = new Swiper(".swiper-container", swiperParams);
 
 request
   .then((response) => response.json())
   .then((data) => {
     data.users.forEach((element) => {
       const div = document.createElement("div");
-      div.classList.add("eventSectionDescSub1");
-
+      div.classList.add("swiper-slide"); // Add swiper-slide class to the div element
       const div1 = document.createElement("div");
       div1.classList.add("eventSectionDescSub1-div");
-
       const img = document.createElement("img");
       const h3 = document.createElement("h3");
-
       const a = document.createElement("a");
       const button = document.createElement("button");
-
       const i = document.createElement("i");
       i.classList.add("fa-solid", "fa-book");
       const span = document.createElement("span");
-
       const div2 = document.createElement("div");
       div2.classList.add("eventSectionDescSub2");
-
       img.src = element.image; // Replace with the actual data property for image URL
-      console.log(element.firstName);
-      console.log(element.lastName);
-
       h3.textContent = element.firstName + " " + element.lastName; // Replace with the actual data property for the specialist's name
-      span.textContent = "  JOIN EVENT"; // Replace with the actual data property for the specialist's description
-
-      // Append the elements to the specialist-img-set div
+      span.textContent = " JOIN EVENT"; // Replace with the actual data property for the specialist's description
+      // Append the elements to the swiper-slide div
       div1.appendChild(img);
       div.appendChild(div1);
-
       button.appendChild(i);
       button.appendChild(span);
-
       a.appendChild(button);
       div2.appendChild(h3);
       div2.appendChild(a);
       div.appendChild(div2);
+      // Append the swiper-slide div to the eventSectionDesc element
       specialistImgSet.appendChild(div);
+      // Update the swiper layout after adding new slides
+      swiper.update();
     });
   })
   .catch((error) => console.log(error.message));
---
+++ scripts/movieCardPage/movieCard.js
@@ -0,0 +1,83 @@
+const urlParam = new URLSearchParams(window.location.search);
+
+const id = Number(urlParam.get("id"));
+console.log(typeof id);
+let filteredData;
+let url = "https://dummyjson.com/users";
+
+const dataProcess = async (url, id) => {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    const newData = data.users.filter((userData) => {
+      if (userData.id == id) {
+        console.log(userData);
+        return userData;
+      }
+    });
+
+    const movieCard = document.querySelector(".movieCard-wrapper");
+
+    const movieCardHeading = document.createElement("div");
+    movieCardHeading.classList.add("movieCardHeading");
+
+    const movieCardDetail = document.createElement("div");
+    movieCardDetail.classList.add("movieCardDetail");
+
+    const movieCardDesc = document.createElement("div");
+    movieCardDesc.classList.add("movieCardDesc");
+
+    const movieCardImg = document.createElement("div");
+    movieCardImg.classList.add("movieCardImg");
+
+    const directorTitle = document.createElement("strong");
+    const director = document.createElement("span");
+    const castTitle = document.createElement("strong");
+    const cast = document.createElement("span");
+    const synopsisTitle = document.createElement("strong");
+    const synopsis = document.createElement("span");
+    const releaseDateTitle = document.createElement("strong");
+    const releaseDate = document.createElement("span");
+
+    const movieCardImage = document.createElement("img");
+    movieCardImage.src = newData[0].image;
+
+    const br1 = document.createElement("br");
+    const br2 = document.createElement("br");
+    const br3 = document.createElement("br");
+
+    movieCardHeading.textContent = newData[0].company.name;
+    directorTitle.innerHTML = "DIRECTOR : ";
+    director.textContent = newData[0].firstName + " " + newData[0].lastName;
+    castTitle.innerHTML = "CAST : ";
+    cast.textContent = newData[0].maidenName;
+    synopsisTitle.textContent = "SYNOPSIS : ";
+    synopsis.textContent = newData[0].userAgent;
+    releaseDateTitle.textContent = "RELEASE DATE : ";
+    releaseDate.textContent = newData[0].birthDate;
+
+    movieCardDesc.appendChild(directorTitle);
+    movieCardDesc.appendChild(director);
+    movieCardDesc.appendChild(br1);
+    movieCardDesc.appendChild(castTitle);
+    movieCardDesc.appendChild(cast);
+    movieCardDesc.appendChild(br2);
+    movieCardDesc.appendChild(synopsisTitle);
+    movieCardDesc.appendChild(synopsis);
+    movieCardDesc.appendChild(br3);
+    movieCardDesc.appendChild(releaseDateTitle);
+    movieCardDesc.appendChild(releaseDate);
+
+    movieCardImg.appendChild(movieCardImage);
+
+    movieCardDetail.appendChild(movieCardDesc);
+    movieCardDetail.appendChild(movieCardImg);
+
+    movieCard.appendChild(movieCardHeading);
+    movieCard.appendChild(movieCardDetail);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+dataProcess(url, id);
