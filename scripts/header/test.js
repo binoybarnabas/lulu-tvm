928953d713b83cc5def9f55f4df5496b21387b9c
Fazil S <fazil.s@experionglobal.local>
2023-11-11 04:11:33 +0530

    fix:added search box

--- scripts/header/test.js
+++ scripts/header/test.js
@@ -15,27 +15,19 @@ let blueFlag = true;
 
 function myFunction(x) {
   x.classList.toggle("change");
-  let div = document.querySelector(".blue");
-  div.classList.toggle("blue-box-active");
+  let div = document.querySelector(".blue-box-active");
+  div.classList.toggle("block");
 
-  if (blueFlag == true) {
-    let blueContentDiv = document.createElement("div");
-    blueContentDiv.classList.add("blue-content-div");
-    blueDivContentArray.forEach((x) => {
-      let blueHrefDiv = document.createElement("div");
-      blueHrefDiv.classList.add("blueContentDiv");
-      let a = document.createElement("a");
-      a.classList.add("remove");
-      a.textContent = x;
-      blueHrefDiv.appendChild(a);
-      blueContentDiv.appendChild(blueHrefDiv);
-      div.appendChild(blueContentDiv);
-    }),
-    blueFlag = false;
-  } else {
-    document.querySelectorAll(".remove").forEach((x) => x.remove());
-    document.querySelectorAll(".blue-content-div").forEach((x) => x.remove());
+  // if (blueFlag == true) {
+  //   blueFlag = false;
+  // } else {
 
-    blueFlag = true;
-  }
+  //   blueFlag = true;
+  // }
+}
+
+
+const input = () => {
+  let inputbutton = document.querySelector(".search-input");
+  inputbutton.classList.toggle("block");
 }
