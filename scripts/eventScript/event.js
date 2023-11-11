718a62166c1feaaeafbb239c809edf750e205cea
Mariyam Fidha Saukath Ali <mariyam.ali@experionglobal.local>
2023-11-11 09:04:56 +0530

    fix:-formatted the code

--- scripts/eventScript/event.js
+++ scripts/eventScript/event.js
@@ -1,83 +1,83 @@
-const url = 'https://dummyjson.com/users';
+// Fetching data from API
 
-    const getData = async(url)=>{
-        try{
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-            data.users.map((userData)=>{
-                showData = document.querySelector('.display');
-                const eventData = createEventData(userData);
-                showData.appendChild(eventData);
-            })
-        }
-        catch(e){
-            console.log(e)
-        }
-        
-    }
+const url = "https://dummyjson.com/users";
 
-    getData(url);
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    data.users.map((userData) => {
+      showData = document.querySelector(".display");
+      const eventData = createEventData(userData);
+      showData.appendChild(eventData);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-    const createEventData = (userData)=>{
+getData(url);
 
-    const eventCards = document.createElement('div');
-    eventCards.classList.add("item");
-    const eventImage = document.createElement('div');
-    eventImage.classList.add("image");
-    const eventDesc = document.createElement('div');
-    eventDesc.classList.add("event-desc");
-    const icon1 = document.createElement('div');
-    icon1.classList.add("icon");
-    const calenderIcon = document.createElement('div');
-    calenderIcon.classList.add("calender-icon");
-    const calender = document.createElement('img');
-    const icon2 = document.createElement('div');
-    icon2.classList.add("icon");   
-    const mapIcon = document.createElement('div');
-    mapIcon.classList.add("map-icon");
-    const map = document.createElement('img');
-    const h2 = document.createElement('h1');
-    const h3 = document.createElement('h1');
-    const eventImg = document.createElement('img');
-    
-    const h1 = document.createElement('h1');
-    const p = document.createElement('p');
-    const button = document.createElement('button');
-    
-    calender.src="../../assets/eventAssets/img/calender.png";
-    map.src="../../assets/eventAssets/img/map.png";
-    eventImg.src= userData.image;
-    h1.textContent = userData.domain;
-    p.textContent = userData.birthDate;
-    h2.textContent = userData.domain;
-    h3.textContent = userData.birthDate;
-    button.textContent="SHOW MORE";
+//Creating elements dynamically
+const createEventData = (userData) => {
+  const eventCards = document.createElement("div");
+  eventCards.classList.add("item");
+  const eventImage = document.createElement("div");
+  eventImage.classList.add("image");
+  const eventDesc = document.createElement("div");
+  eventDesc.classList.add("event-desc");
+  const icon1 = document.createElement("div");
+  icon1.classList.add("icon");
+  const calenderIcon = document.createElement("div");
+  calenderIcon.classList.add("calender-icon");
+  const calender = document.createElement("img");
+  const icon2 = document.createElement("div");
+  icon2.classList.add("icon");
+  const mapIcon = document.createElement("div");
+  mapIcon.classList.add("map-icon");
+  const map = document.createElement("img");
+  const h2 = document.createElement("h1");
+  const h3 = document.createElement("h1");
+  const eventImg = document.createElement("img");
+  const h1 = document.createElement("h1");
+  const p = document.createElement("p");
+  const button = document.createElement("button");
 
-    eventImage.appendChild(eventImg);
-    eventDesc.appendChild(h1);
-    calenderIcon.appendChild(calender);
-    icon1.appendChild(calenderIcon);
-    icon1.appendChild(h2);
-    mapIcon.appendChild(map);
-    icon2.appendChild(mapIcon);
-    icon2.appendChild(h3);
-    eventDesc.appendChild(icon1);
-    eventDesc.appendChild(icon2);
-    eventDesc.appendChild(p); 
-    eventDesc.appendChild(button);
-    eventCards.appendChild(eventImage);
-    eventCards.appendChild(eventDesc);
-    
+  //Storing values into elements
+  calender.src = "../../assets/eventAssets/img/calender.png";
+  map.src = "../../assets/eventAssets/img/map.png";
+  eventImg.src = userData.image;
+  h1.textContent = userData.firstName;
+  p.textContent = userData.birthDate;
+  h2.textContent = userData.lastName;
+  h3.textContent = userData.birthDate;
+  button.textContent = "SHOW MORE";
 
-    button.addEventListener('click',()=>{
+  //Appending element to parent elements
+  eventImage.appendChild(eventImg);
+  eventDesc.appendChild(h1);
+  calenderIcon.appendChild(calender);
+  icon1.appendChild(calenderIcon);
+  icon1.appendChild(h2);
+  mapIcon.appendChild(map);
+  icon2.appendChild(mapIcon);
+  icon2.appendChild(h3);
+  eventDesc.appendChild(icon1);
+  eventDesc.appendChild(icon2);
+  eventDesc.appendChild(p);
+  eventDesc.appendChild(button);
+  eventCards.appendChild(eventImage);
+  eventCards.appendChild(eventDesc);
+
+  //EventListener to go to new page for the required ID
+  button.addEventListener("click", () => {
     openNewPage(userData);
     console.log("hello event");
-    })
-    return eventCards;
+  });
+  return eventCards;
+};
 
-    }
-
-    const openNewPage = (userData)=>{
-        window.location.href = `eventInner.html?id=${userData.id}`
-    }
+const openNewPage = (userData) => {
+  window.location.href = `eventInner.html?id=${userData.id}`;
+};
--- scripts/serviceScript/services.js
+++ scripts/serviceScript/services.js
@@ -1,65 +1,55 @@
-const url = 'https://dummyjson.com/users';
+// Fetching data from API
 
-    const getData = async(url)=>{
-        try{
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-            data.users.map((userData)=>{
-                showData = document.querySelector('.display');
-                const serviceData = createServiceData(userData);
-                showData.appendChild(serviceData);
-            })
-        }
-        catch(e){
-            console.log(e)
-        }
-        
-    }
+const url = "https://dummyjson.com/users";
 
-    getData(url);
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    data.users.map((userData) => {
+      showData = document.querySelector(".display");
+      const serviceData = createServiceData(userData);
+      showData.appendChild(serviceData);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-    const createServiceData = (userData)=>{
+getData(url);
 
-    // const serviceItem = document.createElement('div');
-    // serviceItem.classList.add("item");
-    const serviceCards = document.createElement('div');
-    serviceCards.classList.add("card");
-    const serviceImg = document.createElement('img');
-    const serviceDesc = document.createElement('div');
-    serviceDesc.classList.add("desc");
-    const icon1 = document.createElement('div');
-    icon1.classList.add("icon");
-    const phoneIcon = document.createElement('img');
-    phoneIcon.classList.add("phone-icon");
-    const h1 = document.createElement('h1');
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
-    
-    serviceImg.src= userData.image;
-    h1.textContent = userData.domain;
-    h2.textContent = userData.birthDate;
-    p.textContent = "Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum"
-    phoneIcon.src="../../assets/serviceAssets/img/phone.png";
+//Creating elements dynamically
+const createServiceData = (userData) => {
+  const serviceCards = document.createElement("div");
+  serviceCards.classList.add("card");
+  const serviceImg = document.createElement("img");
+  const serviceDesc = document.createElement("div");
+  serviceDesc.classList.add("desc");
+  const icon1 = document.createElement("div");
+  icon1.classList.add("icon");
+  const phoneIcon = document.createElement("img");
+  phoneIcon.classList.add("phone-icon");
+  const h1 = document.createElement("h1");
+  const h2 = document.createElement("h2");
+  const p = document.createElement("p");
 
-    icon1.appendChild(phoneIcon);
-    icon1.appendChild(h2);
-    serviceDesc.appendChild(h1);
-    serviceDesc.appendChild(icon1);
-    serviceDesc.appendChild(p);
-    serviceCards.appendChild(serviceImg);
-    serviceCards.appendChild(serviceDesc);
-    // serviceItem.appendChild(serviceCards);
-  
+  //Storing values into elements
+  serviceImg.src = userData.image;
+  h1.textContent = userData.domain;
+  h2.textContent = userData.birthDate;
+  p.textContent =
+    "Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum";
+  phoneIcon.src = "../../assets/serviceAssets/img/phone.png";
 
-    // serviceCards.addEventListener('click',()=>{
-    // openNewPage(userData);
-    // console.log("hello event");
-    // })
-    return serviceCards;
+  //Appending element to parent elements
+  icon1.appendChild(phoneIcon);
+  icon1.appendChild(h2);
+  serviceDesc.appendChild(h1);
+  serviceDesc.appendChild(icon1);
+  serviceDesc.appendChild(p);
+  serviceCards.appendChild(serviceImg);
+  serviceCards.appendChild(serviceDesc);
 
-    }
-
-    // const openNewPage = (userData)=>{
-    //     window.location.href = `offersInner.html?id=${userData.id}`
-    // }
+  return serviceCards;
+};
