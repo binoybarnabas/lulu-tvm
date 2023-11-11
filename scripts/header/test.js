9b0be8515e89e37f47eb2f9ceed7124fca03ad45
Fazil S <fazil.s@experionglobal.local>
2023-11-11 10:15:39 +0530

    fix:completed funtura and pvr, fixed search button in header

--- scripts/header/test.js
+++ scripts/header/test.js
@@ -1,33 +1,12 @@
-const blueDivContentArray = [
-  "About Us",
-  "Lulu International",
-  "E-Magazine",
-  "Services",
-  "Floor Plan",
-  "Careers",
-  "Licenses & Permits",
-  "Mall Timings",
-  "Contact Us",
-];
 
-const links = ["https://facebook.com/", "test.com"];
-let blueFlag = true;
 
 function myFunction(x) {
   x.classList.toggle("change");
   let div = document.querySelector(".blue-box-active");
   div.classList.toggle("block");
-
-  // if (blueFlag == true) {
-  //   blueFlag = false;
-  // } else {
-
-  //   blueFlag = true;
-  // }
 }
-
 
 const input = () => {
   let inputbutton = document.querySelector(".search-input");
   inputbutton.classList.toggle("block");
-}
+};
