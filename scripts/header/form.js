e65e34087513727f61c3b9966c1bfc087eb6c244
Fazil S <fazil.s@experionglobal.local>
2023-11-22 11:54:25 +0530

    footer dynamic almost

--- scripts/header/form.js
+++ scripts/header/form.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".contact-form");
-    var data = new Date();
     document.getElementById('date').value = Date();
 
   if (form) {
@@ -10,12 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
       try {
         const formData = new FormData(form);
           console.log(formData);
-          console.log(data);
         const response = await fetch(
           "https://script.google.com/macros/s/AKfycbwq25rzqXqiqlijZoZhOzen8JA86IAsc7uc12wh9ZI23qERYuBBrCv3o3pdYGO5v5Xjkw/exec",
           {
             method: "POST",
-              body: formData, data
+              body: formData,
           }
         );
 
