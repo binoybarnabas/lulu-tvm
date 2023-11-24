d7584805e2aa4c35707aceb57190f60e006552ce
mariyamfidha7 <133568286+mariyamfidha7@users.noreply.github.com>
2023-11-24 18:46:13 +0530

    fix: added headers and footer, changed inner event api

--- scripts/eventScript/eventInner.js
+++ scripts/eventScript/eventInner.js
@@ -23,9 +23,8 @@ const dataProcess = async (url, id) => {
     document.getElementById("h4").innerHTML = newData[0].name;
     document.getElementById("h1").innerHTML = newData[0].name;
     document.getElementById("h2").innerHTML = "About the event";
-    document.getElementById("p").innerHTML =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ";
-    document.getElementById("h3").innerHTML = "Date : " + newData[0].birthDate;
+    document.getElementById("p").innerHTML = newData[0].desc;
+    document.getElementById("h3").innerHTML = "Date : " + newData[0].date;
   } catch (e) {
     console.log(e);
   }
