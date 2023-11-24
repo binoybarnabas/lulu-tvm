9cbe9c1173f3964cd71137611c4b76e9ecead8b8
Mariyam Fidha Saukath Ali <mariyam.ali@experionglobal.local>
2023-11-24 12:51:59 +0530

    fix:

--- scripts/offerScript/offersInner.js
+++ scripts/offerScript/offersInner.js
@@ -5,25 +5,25 @@ const urlParam = new URLSearchParams(window.location.search);
 const id = Number(urlParam.get("id"));
 console.log(typeof id);
 let filteredData;
-let url = "https://dummyjson.com/users";
+let url = "https://mocki.io/v1/3f4629fa-d504-4c46-8514-3b490d9553e2";
 
 //Fetching data of the particular ID
 const dataProcess = async (url, id) => {
   try {
     let response = await fetch(url);
     let data = await response.json();
-    const newData = data.users.filter((userData) => {
+    const newData = data.filter((userData) => {
       if (userData.id == id) {
         return userData;
       }
     });
 
     //Displaying details
-    document.getElementById("img").src = newData[0].image;
-    document.getElementById("h4").innerHTML = newData[0].firstName;
-    document.getElementById("h1").innerHTML = newData[0].firstName;
-    document.getElementById("h2").innerHTML = newData[0].company.department;
-    document.getElementById("h3").innerHTML = newData[0].birthDate;
+    document.getElementById("img").src = newData[0].url;
+    document.getElementById("h4").innerHTML = newData[0].name;
+    document.getElementById("h1").innerHTML = newData[0].name;
+    document.getElementById("h2").innerHTML = newData[0].offer;
+    document.getElementById("h3").innerHTML = newData[0].date;
     document.getElementById("p").innerHTML =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ";
   } catch (e) {
--- scripts/serviceScript/services.js
+++ scripts/serviceScript/services.js
@@ -1,3 +1,4 @@
+"use strict";
 // Fetching data from API
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -10,7 +11,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 // const url = "https://dummyjson.com/users";
 const url = "https://mocki.io/v1/507f2920-a1e6-4006-960c-af0c1a9fd2ac";
-const getData = (url) => __awaiter(this, void 0, void 0, function* () {
+const getData = (url) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const response = yield fetch(url);
         const data = yield response.json();
