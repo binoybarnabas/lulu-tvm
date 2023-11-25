15a3c5031c65aac673f7e9cfa918558d212818f4
mariyamfidha7 <133568286+mariyamfidha7@users.noreply.github.com>
2023-11-25 17:03:19 +0530

    careers and service page fix

---
+++ scripts/eventScript/event.js
@@ -0,0 +1,162 @@
+// Fetching data from API
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// const url = "https://dummyjson.com/users";
+const url = "https://mocki.io/v1/f940fb9c-57a4-4207-9ca9-8b2bf05c313c";
+const getData = (url) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const response = yield fetch(url);
+        const data = yield response.json();
+        console.log(data);
+        data.map((userData) => {
+            const showData = document.querySelector(".display");
+            const eventData = createEventData(userData);
+            showData.appendChild(eventData);
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+});
+getData(url);
+// Creating elements dynamically
+const createEventData = (userData) => {
+    const eventCards = document.createElement("div");
+    eventCards.classList.add("item");
+    const eventImage = document.createElement("div");
+    eventImage.classList.add("image");
+    const eventDesc = document.createElement("div");
+    eventDesc.classList.add("event-desc");
+    const icon1 = document.createElement("div");
+    icon1.classList.add("icon");
+    const calenderIcon = document.createElement("div");
+    calenderIcon.classList.add("calender-icon");
+    const calender = document.createElement("img");
+    const icon2 = document.createElement("div");
+    icon2.classList.add("icon");
+    const mapIcon = document.createElement("div");
+    mapIcon.classList.add("map-icon");
+    const map = document.createElement("img");
+    const h2 = document.createElement("h1");
+    const h3 = document.createElement("h1");
+    const eventImg = document.createElement("img");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+    const button = document.createElement("button");
+    // Storing values into elements
+    calender.src = "../../assets/eventAssets/img/calender.png";
+    map.src = "../../assets/eventAssets/img/map.png";
+    eventImg.src = userData.url;
+    h1.textContent = userData.name;
+    p.textContent = userData.desc;
+    h2.textContent = userData.date;
+    h3.textContent = userData.place;
+    button.textContent = "SHOW MORE";
+    // Appending element to parent elements
+    eventImage.appendChild(eventImg);
+    eventDesc.appendChild(h1);
+    calenderIcon.appendChild(calender);
+    icon1.appendChild(calenderIcon);
+    icon1.appendChild(h2);
+    mapIcon.appendChild(map);
+    icon2.appendChild(mapIcon);
+    icon2.appendChild(h3);
+    eventDesc.appendChild(icon1);
+    eventDesc.appendChild(icon2);
+    eventDesc.appendChild(p);
+    eventDesc.appendChild(button);
+    eventCards.appendChild(eventImage);
+    eventCards.appendChild(eventDesc);
+    // EventListener to go to a new page for the required ID
+    button.addEventListener("click", () => {
+        openNewPage(userData);
+        console.log("hello event");
+    });
+    return eventCards;
+};
+const openNewPage = (userData) => {
+    window.location.href = `eventInner.html?id=${userData.id}`;
+};
+// Fetching data from API
+// const url = "https://dummyjson.com/users";
+// const url = "https://mocki.io/v1/f940fb9c-57a4-4207-9ca9-8b2bf05c313c";
+// const getData = async (url) => {
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+//     data.map((userData) => {
+//       showData = document.querySelector(".display");
+//       const eventData = createEventData(userData);
+//       showData.appendChild(eventData);
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+// getData(url);
+//Creating elements dynamically
+// const createEventData = (userData) => {
+//   const eventCards = document.createElement("div");
+//   eventCards.classList.add("item");
+//   const eventImage = document.createElement("div");
+//   eventImage.classList.add("image");
+//   const eventDesc = document.createElement("div");
+//   eventDesc.classList.add("event-desc");
+//   const icon1 = document.createElement("div");
+//   icon1.classList.add("icon");
+//   const calenderIcon = document.createElement("div");
+//   calenderIcon.classList.add("calender-icon");
+//   const calender = document.createElement("img");
+//   const icon2 = document.createElement("div");
+//   icon2.classList.add("icon");
+//   const mapIcon = document.createElement("div");
+//   mapIcon.classList.add("map-icon");
+//   const map = document.createElement("img");
+//   const h2 = document.createElement("h1");
+//   const h3 = document.createElement("h1");
+//   const eventImg = document.createElement("img");
+//   const h1 = document.createElement("h1");
+//   const p = document.createElement("p");
+//   const button = document.createElement("button");
+//   //Storing values into elements
+//   calender.src = "../../assets/eventAssets/img/calender.png";
+//   map.src = "../../assets/eventAssets/img/map.png";
+//   eventImg.src = userData.url;
+//   h1.textContent = userData.name;
+//   p.textContent = userData.desc;
+//   h2.textContent = userData.date;
+//   h3.textContent = userData.place;
+//   button.textContent = "SHOW MORE";
+//Appending element to parent elements
+// eventImage.appendChild(eventImg);
+// eventDesc.appendChild(h1);
+// calenderIcon.appendChild(calender);
+// icon1.appendChild(calenderIcon);
+// icon1.appendChild(h2);
+// mapIcon.appendChild(map);
+// icon2.appendChild(mapIcon);
+// icon2.appendChild(h3);
+// eventDesc.appendChild(icon1);
+// eventDesc.appendChild(icon2);
+// eventDesc.appendChild(p);
+// eventDesc.appendChild(button);
+// eventCards.appendChild(eventImage);
+// eventCards.appendChild(eventDesc);
+//EventListener to go to new page for the required ID
+//   button.addEventListener("click", () => {
+//     openNewPage(userData);
+//     console.log("hello event");
+//   });
+//   return eventCards;
+// };
+// const openNewPage = (userData) => {
+//   window.location.href = `eventInner.html?id=${userData.id}`;
+// };
