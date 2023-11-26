fe95bdfb77bffbfd38b77e2b1d5bf73d65ae23e3
fazilsubair <fazilsubair14@gmail.com>
2023-11-26 23:34:50 +0530

    entertainments page

--- scripts/entertainment/entertainment.js
+++ scripts/entertainment/entertainment.js
@@ -1,9 +1,10 @@
-const url = "https://dummyjson.com/users";
+const url = "https://mocki.io/v1/5358c6a4-322a-43e4-a69e-7954aedf4a77";
 
 const getData = async (url) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data[0])
     entertainmetCards(data);
   } catch (x) {
     console.log(x);
@@ -13,32 +14,29 @@ const getData = async (url) => {
 getData(url);
 
 const entertainmetCards = (data) => {
-  let rand1 = Math.floor(Math.random() * 9);
-  let rand2 = Math.floor(Math.random() * 9);
 
-  console.log(data.users[rand1].image);
+
+  // console.log(data.users[rand1].image);
   let img1 = document.getElementById("img1");
-  img1.src = data.users[rand1].image;
+  img1.src = data[0].image;
   let card1 = document.querySelector(".card1");
-  document.querySelector("#card1").textContent = data.users[rand1].company.name 
+  document.querySelector("#card1").textContent = data[0].name 
   card1.addEventListener("click", () => {
-    // console.log("link working");
-    window.location.href = `funtura.html?id=${data.users[rand1].id}`;
+    console.log("link working");
+    window.location.href = `pvr.html`;
   });
   img1.style.backgroundColor = "blue";
   let img2 = document.getElementById("img2");
-  img2.src = data.users[rand2].image;
+  img2.src =  data[1].image;
   img2.style.backgroundColor = "blue";
-  img2.src = data.users[rand2].image;
   let card2 = document.querySelector(".card2");
-  document.querySelector("#card2").textContent = data.users[rand2].company.name 
+  document.querySelector("#card2").textContent = data[1].name 
   card2.addEventListener("click", () => {
-    // console.log("link working");
-    // console.log(rand2);
-    window.location.href = `funtura.html?id=${data.users[rand2].id}`;
+    console.log("link working");
+    window.location.href = `funtura.html`;
   });
 };
 
-// const openNewpage = (data) => {
-//   window.location.href = `funtura.html`
-// }
+const openNewpage = (data) => {
+  window.location.href = `funtura.html`
+}
